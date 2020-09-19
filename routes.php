8d1e2e9d3d24b05e27efd0dee4a6e820d35bdfd7
soncrtoitoi9999 <71562001+soncrtoitoi9999@users.noreply.github.com>
2020-09-19 23:19:49 +0700

    Add files via upload

---
+++ routes.php
@@ -0,0 +1,16 @@
+<?php
+$controllers = array(
+  'pages' => ['home', 'error'],
+  'posts' => ['index'],
+);
+
+if (!array_key_exists($controller, $controllers) || !in_array($action, $controllers[$controller])) {
+  $controller = 'pages';
+  $action = 'error';
+}
+
+include_once('controllers/' . $controller . '_controller.php');
+$klass = str_replace('_', '', ucwords($controller, '_')) . 'Controller';
+$controller = new $klass;
+$controller->$action();
+?>
